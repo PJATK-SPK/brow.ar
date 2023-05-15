8c02ee5f07ad2a3a7c10c5bd349fd57f77628aad
h4570 <sandro.sobczynski@gmail.com>
2023-05-15 22:14:55 +0200

    init

---
+++ Browar/app/src/main/java/com/example/browar/BMI.java
@@ -0,0 +1,101 @@
+// TODO PAWEL - Authors
+
+package com.example.browar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class BMI extends AppCompatActivity {
+
+    private static final NumberFormat formatter = NumberFormat.getNumberInstance();
+
+    private double userWeight = 0.000;
+    private double userHeight = 0;
+
+    private TextView weightTextView;
+    private TextView heightTextView;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bmi);
+
+        weightTextView = (TextView) findViewById(R.id.weightTextView);
+        heightTextView = (TextView) findViewById(R.id.heightTextView);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+        // set amountEditText's TextWatcher
+        EditText field1 = (EditText) findViewById(R.id.weightEditText);
+        field1.addTextChangedListener(weightEditTextWatcher);
+
+        EditText field2 = (EditText) findViewById(R.id.heightEditText);
+        field2.addTextChangedListener(heightEditTextWatcher);
+
+        Button returnButton = findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BMI.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void calculate() {
+        double result = userWeight / Math.pow(userHeight / 100.0, 2);
+        resultTextView.setText(formatter.format(result));
+    }
+
+    private final TextWatcher heightEditTextWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                userHeight = Double.parseDouble(s.toString());
+                heightTextView.setText(formatter.format(userHeight));
+            }
+            catch (NumberFormatException e) {
+                heightTextView.setText("");
+                userHeight = 0;
+            }
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+    };
+
+    private final TextWatcher weightEditTextWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                userWeight = Double.parseDouble(s.toString());
+                weightTextView.setText(formatter.format(userWeight));
+            }
+            catch (NumberFormatException e) {
+                weightTextView.setText("");
+                userWeight = 0.0;
+            }
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+    };
+}
---
+++ Browar/app/src/main/java/com/example/browar/Start.java
@@ -0,0 +1,29 @@
+// TODO PAWEL - Authors
+
+package com.example.browar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+
+        Button nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Start.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
