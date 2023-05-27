8709a987ce4cb34c740a2d37aa60a0cb1c63622c
h4570 <sandro.sobczynski@gmail.com>
2023-05-27 23:53:39 +0200

    add get beers endpoint

--- backend/endpoints/get_beers.js
+++ backend/endpoints/get_beers.js
@@ -4,7 +4,12 @@ class GetBeers {
 
     execute(req, res) {
         const db = new Database();
-        db.getBeers().then((result) => res.status(200).json(result));
+
+        const sql = `
+            select b.id, b.name, b.image_url, b.is_mock
+            from core.beers b`;
+
+        db.query(sql).then((result) => res.status(200).json(result));
     }
 
 }
