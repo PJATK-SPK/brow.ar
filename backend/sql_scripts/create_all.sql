01e2c03fd91642fb452227abf4f8e73bb3c7abeb
h4570 <sandro.sobczynski@gmail.com>
2023-05-27 23:54:33 +0200

    add is_mock columns

--- backend/sql_scripts/create_all.sql
+++ backend/sql_scripts/create_all.sql
@@ -9,6 +9,7 @@ CREATE TABLE core.beers (
 	description varchar(3000) not null,
 	image_url varchar(300) not null,
 	manufacturer_id int null,
+    is_mock BOOLEAN DEFAULT TRUE NOT NULL,
 	FOREIGN KEY (manufacturer_id) REFERENCES manufacturers(id)
 );
 
@@ -18,6 +19,7 @@ CREATE TABLE core.rates (
     taste_rating INT CHECK (taste_rating >= 1 AND taste_rating <= 5),
     color_rating INT CHECK (color_rating >= 1 AND color_rating <= 5),
     aroma_rating INT CHECK (aroma_rating >= 1 AND aroma_rating <= 5),
+    is_mock BOOLEAN DEFAULT TRUE NOT NULL,
     FOREIGN KEY (beer_id) REFERENCES beers(id)
 );
 
@@ -25,5 +27,6 @@ CREATE TABLE core.comments (
     id SERIAL PRIMARY KEY,
     beer_id INT NOT NULL,
     content varchar(3000) not null,
+    is_mock BOOLEAN DEFAULT TRUE NOT NULL,
     FOREIGN KEY (beer_id) REFERENCES beers(id)
 );
