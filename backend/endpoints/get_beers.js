a056525d62dd2662d85de76deda64a1dbf9a5b1c
h4570 <sandro.sobczynski@gmail.com>
2023-05-27 19:04:05 +0200

    add first endpoint

---
+++ backend/endpoints/get_beers.js
@@ -0,0 +1,9 @@
+class GetBeers {
+
+    execute(req, res) {
+        res.send('Hello World!');
+    }
+
+}
+
+export default GetBeers;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const path = require('path')
+// Imports
+import GetBeers from "./endpoints/get_beers.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-const PORT = process.env.PORT || 5001
+const app = express();
+const port = process.env.PORT || 5001;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Endpoints
+app.get('/beers', (req, res) => new GetBeers().execute(req, res));
+
+// Configuration
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(port, () => console.log(`App listening on port ${port}!`));
