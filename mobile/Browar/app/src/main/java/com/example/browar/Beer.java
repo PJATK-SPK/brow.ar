a4e2ac4deeae07c1c70d985717fe8bbc06826d3f
marcel <marcel.pankanin@ramp.network>
2023-05-30 17:25:42 +0200

    remove beer

--- mobile/Browar/app/src/main/java/com/example/browar/Beer.java
+++ mobile/Browar/app/src/main/java/com/example/browar/Beer.java
@@ -91,8 +91,6 @@ public class Beer extends AppCompatActivity {
                                         // obsłuż błąd
                                     }
                                 });
-
-
                             }
                     });
 
